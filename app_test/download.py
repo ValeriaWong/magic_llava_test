39ed99160af99e0b155375578e3d8843a91a4a66
Nobody-ML <1755309985@qq.com>
2024-06-10 17:28:43 +0800

    update

--- app_test/download.py
+++ app_test/download.py
@@ -1,3 +1,6 @@
-from openxlab.model import download
+# from openxlab.model import download
 
-download(model_repo='Nobody-ML/internvl-1_5-2b', output='/home/xlab-app-center')
+# download(model_repo='Nobody-ML/internvl-1_5-2b', output='/home/xlab-app-center')
+
+from modelscope import snapshot_download
+model_dir = snapshot_download('OpenGVLab/Mini-InternVL-Chat-2B-V1-5', cache_dir='/home/xlab-app-center')
