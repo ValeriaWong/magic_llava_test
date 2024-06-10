dbf8948f8131bb4d44b8de5331586f646072e2f6
Nobody-ML <128660843+Nobody-ML@users.noreply.github.com>
2024-06-11 01:51:51 +0800

    Update app.py

--- app_test/app.py
+++ app_test/app.py
@@ -16,11 +16,12 @@ os.system("python app_test/download.py")
 # os.system("cd /home/xlab-app-center/app_test/internvl_chat_llava")
 # os.system("python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload")
 
-os.system("cd /home/xlab-app-center/app_test/internvl_chat_llava/ && python -m llava.serve.controller --host 0.0.0.0 --port 10000")
+# os.system("cd /home/xlab-app-center/app_test/internvl_chat_llava/ && python -m llava.serve.controller --host 0.0.0.0 --port 10000")
 # os.system("python -m llava.serve.controller --host 0.0.0.0 --port 10000")
 
-os.system("cd /home/xlab-app-center/app_test/internvl_chat/ && python -m internvl.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40005 --worker http://localhost:40005 --model-path /home/xlab-app-center")
+# os.system("cd /home/xlab-app-center/app_test/internvl_chat/ && python -m internvl.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40005 --worker http://localhost:40005 --model-path /home/xlab-app-center")
 # os.system("python -m internvl.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40005 --worker http://localhost:40005 --model-path /home/xlab-app-center")
 
+os.system("./create.sh")
 os.system("cd /home/xlab-app-center/app_test/internvl_chat_llava/ && python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload")
 # os.system("python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload")
